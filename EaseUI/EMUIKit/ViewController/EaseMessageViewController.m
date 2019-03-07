5a6ae2a3179875eff28d5f080b6d2178c07f0b31
tangchuanhui <tangchuanhui@huban360.com>
2019-03-07 14:12:01 +0800

    s

--- EaseUI/EMUIKit/ViewController/EaseMessageViewController.m
+++ EaseUI/EMUIKit/ViewController/EaseMessageViewController.m
@@ -32,6 +32,46 @@
 
 #define IOS_VERSION [[UIDevice currentDevice] systemVersion]>=9.0
 
+
+@implementation UIImage (GIF)
+
++ (UIImage *)sd_animatedGIFNamed:(NSString *)name {
+    CGFloat scale = [UIScreen mainScreen].scale;
+    
+    if (scale > 1.0f) {
+        NSString *retinaPath = [[NSBundle mainBundle] pathForResource:[name stringByAppendingString:@"@2x"] ofType:@"gif"];
+        
+        NSData *data = [NSData dataWithContentsOfFile:retinaPath];
+        
+        if (data) {
+            return [UIImage sd_animatedGIFWithData:data];
+        }
+        
+        NSString *path = [[NSBundle mainBundle] pathForResource:name ofType:@"gif"];
+        
+        data = [NSData dataWithContentsOfFile:path];
+        
+        if (data) {
+            return [UIImage sd_animatedGIFWithData:data];
+        }
+        
+        return [UIImage imageNamed:name];
+    }
+    else {
+        NSString *path = [[NSBundle mainBundle] pathForResource:name ofType:@"gif"];
+        
+        NSData *data = [NSData dataWithContentsOfFile:path];
+        
+        if (data) {
+            return [UIImage sd_animatedGIFWithData:data];
+        }
+        
+        return [UIImage imageNamed:name];
+    }
+}
+
+@end
+
 typedef enum : NSUInteger {
     EMRequestRecord,
     EMCanRecord,
